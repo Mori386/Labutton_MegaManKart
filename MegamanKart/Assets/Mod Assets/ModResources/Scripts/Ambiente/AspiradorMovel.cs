4701600fb806600803247cfe10ec85e80851d551
RMorizono <rkmorizono@gmail.com>
2022-03-12 17:02:33 -0300

    Script de mover objetos no cenário

---
+++ MegamanKart/Assets/Mod Assets/ModResources/Scripts/Ambiente/AspiradorMovel.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AspiradorMovel : MonoBehaviour
+{
+    Vector3 posicaoInicial;
+    [SerializeField] Vector3 posicaoFinal;
+    [SerializeField] float velocidade;
+    private void Start()
+    {
+        StartCoroutine(MoverObjeto(posicaoFinal));
+    }
+    private IEnumerator MoverObjeto(Vector3 localFinal)
+    {
+        Vector3 localInicial = transform.position;
+        Vector3 direcao;
+        direcao = Vector3.Normalize(localFinal - transform.position);
+        while(Vector3.Distance(transform.position,localFinal)>0.01)
+        {
+            transform.position += direcao * velocidade * Time.fixedDeltaTime;
+            yield return new WaitForFixedUpdate();
+        }
+        StartCoroutine(MoverObjeto(localInicial));
+        yield break;
+    }
+}
