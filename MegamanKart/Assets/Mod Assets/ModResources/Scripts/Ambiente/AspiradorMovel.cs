8853b083801da2e1c990d5df1ddca1389d2dc8a5
RMorizono <rkmorizono@gmail.com>
2022-03-17 18:42:14 -0300

    AspiradorMover Script BugFix

--- MegamanKart/Assets/Mod Assets/ModResources/Scripts/Ambiente/AspiradorMovel.cs
+++ MegamanKart/Assets/Mod Assets/ModResources/Scripts/Ambiente/AspiradorMovel.cs
@@ -15,10 +15,19 @@ public class AspiradorMovel : MonoBehaviour
         Vector3 localInicial = transform.position;
         Vector3 direcao;
         direcao = Vector3.Normalize(localFinal - transform.position);
-        while(Vector3.Distance(transform.position,localFinal)>0.01)
+        Vector3 posicaoDelta = localFinal - localInicial;
+        float distanciaAPercorrer = Mathf.Abs(posicaoDelta.x) + Mathf.Abs(posicaoDelta.y) + Mathf.Abs(posicaoDelta.z);
+        Vector3 distanciaVetorialPorSegundo= direcao * velocidade * Time.fixedDeltaTime;
+        float distanciaTotalPorTick = Mathf.Abs(distanciaVetorialPorSegundo.x) + Mathf.Abs(distanciaVetorialPorSegundo.y) + Mathf.Abs(distanciaVetorialPorSegundo.z);
+        while (distanciaAPercorrer > distanciaTotalPorTick)
         {
+            distanciaAPercorrer -= distanciaTotalPorTick;
             transform.position += direcao * velocidade * Time.fixedDeltaTime;
             yield return new WaitForFixedUpdate();
+        }
+        if(distanciaAPercorrer > 0)
+        {
+            transform.position = localFinal;
         }
         StartCoroutine(MoverObjeto(localInicial));
         yield break;
