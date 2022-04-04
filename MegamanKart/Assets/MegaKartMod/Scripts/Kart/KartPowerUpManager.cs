c2a33cb8a4ffcb120431f6ec44cc8a158410a5bd
Rodrigo Morizono <rkmorizono@gmail.com>
2022-04-04 11:25:06 -0300

    Update KartPowerUpManager.cs

--- MegamanKart/Assets/MegaKartMod/Scripts/Kart/KartPowerUpManager.cs
+++ MegamanKart/Assets/MegaKartMod/Scripts/Kart/KartPowerUpManager.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class KartPowerUpManager : MonoBehaviour
 {
     [SerializeField] int maximoDeItensCarregados;
-    public enum listPowerUps  
+    public enum listPowerUps
     {
-        Empty,Blaster, Blade, Shield
+        Empty = 0,
+        Missel = 1,
+        Blade = 2,
+        Shield = 3,
+        LandMine = 4,
+        Hack = 5
     }
     [System.NonSerialized] public listPowerUps[] powerUpsAtuais;
     private void Awake()
@@ -18,11 +23,68 @@ public class KartPowerUpManager : MonoBehaviour
             powerUpsAtuais[i] = listPowerUps.Empty;
         }
     }
-    public void PickUpPowerUp()
+    public void UpdatePowerupUse()
     {
-        for(int i = 0; i<maximoDeItensCarregados;i++)
+        switch ((int)powerUpsAtuais[0])
         {
-            
+            case 0:
+                PowerUpUse = null;
+                break;
+            case 1:
+                PowerUpUse = MisselUse;
+                break;
+            case 2:
+                PowerUpUse = BladeUse;
+                break;
+            case 3:
+                PowerUpUse = BladeUse;
+                break;
+            case 4:
+                PowerUpUse = LandMineUse;
+                break;
+            case 5:
+                PowerUpUse = HackUse;
+                break;
         }
+    }
+    private void MovetoNextItemSlot()
+    {
+        for (int i = 0; i < maximoDeItensCarregados; i++)
+        {
+            if(i+1<maximoDeItensCarregados)powerUpsAtuais[i] = powerUpsAtuais[i+1];
+        }
+    }
+    public delegate void KartPowerUpAvailiabes();
+    public KartPowerUpAvailiabes PowerUpUse;
+    public void PickUpPowerUp(listPowerUps powerUp)
+    {
+        for (int i = 0; i < maximoDeItensCarregados; i++)
+        {
+            if (powerUpsAtuais[i] == 0)
+            {
+                powerUpsAtuais[i] = powerUp;
+                break;
+            }
+        }
+    }
+    public void MisselUse()
+    {
+
+    }
+    public void BladeUse()
+    {
+
+    }
+    public void ShieldUse()
+    {
+
+    }
+    public void LandMineUse()
+    {
+
+    }
+    public void HackUse()
+    {
+
     }
 }
